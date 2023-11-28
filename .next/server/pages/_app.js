5ce1edc39d4b2c2cb15a1af9138fa9bf90ba5d2e
OreoluwaF <oresinbox@hotmail.com>
2023-11-28 00:36:42 +0000

    demo2

--- .next/server/pages/_app.js
+++ .next/server/pages/_app.js
@@ -1,1 +1,68 @@
-(()=>{var r={};r.id=888,r.ids=[888],r.modules={9597:(r,e,t)=>{"use strict";t.r(e),t.d(e,{default:()=>u});var s=t(997);function MyApp({Component:r,pageProps:e}){return s.jsx(r,{...e})}t(6689),t(5931),t(6764);let u=MyApp},5931:()=>{},6764:()=>{},6689:r=>{"use strict";r.exports=require("react")},997:r=>{"use strict";r.exports=require("react/jsx-runtime")}};var e=require("../webpack-runtime.js");e.C(r);var t=e(e.s=9597);module.exports=t})();
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+(() => {
+var exports = {};
+exports.id = "pages/_app";
+exports.ids = ["pages/_app"];
+exports.modules = {
+
+/***/ "./pages/_app.js":
+/*!***********************!*\
+  !*** ./pages/_app.js ***!
+  \***********************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n/* harmony import */ var react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react/jsx-dev-runtime */ \"react/jsx-dev-runtime\");\n/* harmony import */ var react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__);\n/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! react */ \"react\");\n/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_1__);\n/* harmony import */ var bootstrap_dist_css_bootstrap_css__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! bootstrap/dist/css/bootstrap.css */ \"./node_modules/bootstrap/dist/css/bootstrap.css\");\n/* harmony import */ var bootstrap_dist_css_bootstrap_css__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(bootstrap_dist_css_bootstrap_css__WEBPACK_IMPORTED_MODULE_2__);\n/* harmony import */ var _styles_globals_css__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ../styles/globals.css */ \"./styles/globals.css\");\n/* harmony import */ var _styles_globals_css__WEBPACK_IMPORTED_MODULE_3___default = /*#__PURE__*/__webpack_require__.n(_styles_globals_css__WEBPACK_IMPORTED_MODULE_3__);\n\n\n // Import Bootstrap CSS\n // Keep your custom CSS import below Bootstrap to override default styles\nfunction MyApp({ Component, pageProps }) {\n    return /*#__PURE__*/ (0,react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxDEV)(Component, {\n        ...pageProps\n    }, void 0, false, {\n        fileName: \"/Users/oreo/React-CSS/Ui-Ux-Test/Code/pages/_app.js\",\n        lineNumber: 6,\n        columnNumber: 10\n    }, this);\n}\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (MyApp);\n//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi9wYWdlcy9fYXBwLmpzIiwibWFwcGluZ3MiOiI7Ozs7Ozs7Ozs7Ozs7QUFBMEI7QUFDZ0IsQ0FBQyx1QkFBdUI7QUFDbkMsQ0FBQyx5RUFBeUU7QUFFekcsU0FBU0MsTUFBTSxFQUFFQyxTQUFTLEVBQUVDLFNBQVMsRUFBRTtJQUNyQyxxQkFBTyw4REFBQ0Q7UUFBVyxHQUFHQyxTQUFTOzs7Ozs7QUFDakM7QUFFQSxpRUFBZUYsS0FBS0EsRUFBQyIsInNvdXJjZXMiOlsid2VicGFjazovL2NvZGUvLi9wYWdlcy9fYXBwLmpzP2UwYWQiXSwic291cmNlc0NvbnRlbnQiOlsiaW1wb3J0IFJlYWN0IGZyb20gJ3JlYWN0JztcbmltcG9ydCAnYm9vdHN0cmFwL2Rpc3QvY3NzL2Jvb3RzdHJhcC5jc3MnOyAvLyBJbXBvcnQgQm9vdHN0cmFwIENTU1xuaW1wb3J0ICcuLi9zdHlsZXMvZ2xvYmFscy5jc3MnOyAvLyBLZWVwIHlvdXIgY3VzdG9tIENTUyBpbXBvcnQgYmVsb3cgQm9vdHN0cmFwIHRvIG92ZXJyaWRlIGRlZmF1bHQgc3R5bGVzXG5cbmZ1bmN0aW9uIE15QXBwKHsgQ29tcG9uZW50LCBwYWdlUHJvcHMgfSkge1xuICByZXR1cm4gPENvbXBvbmVudCB7Li4ucGFnZVByb3BzfSAvPjtcbn1cblxuZXhwb3J0IGRlZmF1bHQgTXlBcHA7XG4iXSwibmFtZXMiOlsiUmVhY3QiLCJNeUFwcCIsIkNvbXBvbmVudCIsInBhZ2VQcm9wcyJdLCJzb3VyY2VSb290IjoiIn0=\n//# sourceURL=webpack-internal:///./pages/_app.js\n");
+
+/***/ }),
+
+/***/ "./styles/globals.css":
+/*!****************************!*\
+  !*** ./styles/globals.css ***!
+  \****************************/
+/***/ (() => {
+
+
+
+/***/ }),
+
+/***/ "react":
+/*!************************!*\
+  !*** external "react" ***!
+  \************************/
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("react");
+
+/***/ }),
+
+/***/ "react/jsx-dev-runtime":
+/*!****************************************!*\
+  !*** external "react/jsx-dev-runtime" ***!
+  \****************************************/
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("react/jsx-dev-runtime");
+
+/***/ })
+
+};
+;
+
+// load runtime
+var __webpack_require__ = require("../webpack-runtime.js");
+__webpack_require__.C(exports);
+var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+var __webpack_exports__ = __webpack_require__.X(0, ["vendor-chunks/bootstrap"], () => (__webpack_exec__("./pages/_app.js")));
+module.exports = __webpack_exports__;
+
+})();
--- components/ProductDisplay.js
+++ components/ProductDisplay.js
@@ -1,31 +1,41 @@
 import React from 'react';
 import Image from 'next/image';
-import styles from '../styles/ProductSection.module.css'; // Adjust the path as necessary
-import chairImage from '../public/chair.png'; // Ensure the path to your chair image is correct
+import styles from '../styles/ProductSection.module.css';
+import chairImage from '../public/chair.png';
 
 const ProductDisplay = () => {
   return (
     <section className={styles.productSection}>
       <div className={styles.productContent}>
-        <div className="row">
-          {/* Switched the order of the columns here */}
-          <div className="col-md-6">
+        <div className={`row ${styles.myCustomGrid}`}>
+          {/* Text Section */}
+          <div className="col-md-6" style={{ order: 1 }}>
             <h1 className={styles.productTitle}>Costway</h1>
             <h2 className={styles.productSubtitle}>Mid-Century Accent Chair</h2>
             <p className={styles.productDescription}>
               The Rivet Julie Mid-Century Modern Chair combines sleek design with comfort.
               Its clean lines and sturdy construction make it an ideal choice for any modern living space.
             </p>
-            {/* Color options and other details */}
-            <div className={styles.colorOptions}>
-              {/* Render color options here */}
+            
+
+          {/* Image Section */}
+          <div className="col-md-6" style={{ order: 2 }}>
+            <div className={styles.productImage}>
+              <Image src={chairImage} alt="Mid-Century Accent Chair" width={300} height={300} />
+            </div>
+          </div>
+          {/* Color options */}
+          <div className={styles.colorOptions}>
               <div className={styles.colorOption} style={{ backgroundColor: 'blue' }}></div>
               <div className={styles.colorOption} style={{ backgroundColor: 'red' }}></div>
               <div className={styles.colorOption} style={{ backgroundColor: 'green' }}></div>
             </div>
+          </div>
+
+          {/* Price and Actions Section */}
+          <div className="col-8" style={{ order: 3 }}>
             <div className={styles.priceRating}>
               <span className={styles.price}>$199.99</span>
-              {/* Rating would be displayed here */}
               <div className={styles.rating}>
                 <span className={styles.filledStar}>★</span>
                 <span className={styles.filledStar}>★</span>
@@ -38,19 +48,12 @@ const ProductDisplay = () => {
             <div className={styles.productActions}>
               <div className="row">
                 <div className="col-6">
-                  {/* Apply the custom styles along with any additional classes as needed */}
                   <button className={`btn btn-outline-primary btn-block ${styles.button} ${styles.buyNow}`}>Buy Now</button>
                 </div>
                 <div className="col-6">
-                  {/* Apply the custom styles along with any additional classes as needed */}
                   <button className={`btn btn-outline-primary btn-block ${styles.button} ${styles.addToCart}`}>Add&nbsp;to&nbsp;Cart</button>
                 </div>
               </div>
-            </div>
-          </div>
-          <div className="col-md-6">
-            <div className={styles.productImage}>
-              <Image src={chairImage} alt="Mid-Century Accent Chair" width={300} height={300} />
             </div>
           </div>
         </div>
